0ff10b7cc213130315ec7f009da5c49634b21414
Petrenko Sergii <petrenko7@yahoo.com>
2025-05-09 08:21:22 +0300

    Add SeedController

--- windpowersystemv5.client/src/app/health-check/health-check.component.ts
+++ windpowersystemv5.client/src/app/health-check/health-check.component.ts
@@ -7,21 +7,25 @@ import { environment } from '../../environments/environment';
   templateUrl: './health-check.component.html',
   styleUrls: ['./health-check.component.scss']
 })
+
 export class HealthCheckComponent implements OnInit {
   public result?: Result;
   constructor(private http: HttpClient) {
   }
+
   ngOnInit() {
     this.http.get<Result>(environment.baseUrl + 'api/health').subscribe(result => {
       this.result = result;
     }, error => console.error(error));
   }
 }
+
 interface Result {
   checks: Check[];
   totalStatus: string;
   totalResponseTime: number;
 }
+
 interface Check {
   name: string;
   responseTime: number;
