5eda09cc8f4bd598e78380fa64471fbe7b90a745
Petrenko Sergii <petrenko7@yahoo.com>
2025-05-23 15:23:51 +0300

    Use GraphQL's query and mutation in CityService

--- windpowersystemv5.client/src/app/cities/city.service.ts
+++ windpowersystemv5.client/src/app/cities/city.service.ts
@@ -27,20 +27,54 @@ export class CityService
     filterColumn: string | null,
     filterQuery: string | null
   ): Observable<ApiResult<City>> {
-    var url = this.getUrl("api/Cities");
-    var params = new HttpParams()
-      .set("pageIndex", pageIndex.toString())
-      .set("pageSize", pageSize.toString())
-      .set("sortColumn", sortColumn)
-      .set("sortOrder", sortOrder);
-
-    if (filterColumn && filterQuery) {
-      params = params
-        .set("filterColumn", filterColumn)
-        .set("filterQuery", filterQuery);
-    }
-
-    return this.http.get<ApiResult<City>>(url, { params });
+    return this.apollo
+      .query({
+        query: gql`
+          query GetCitiesApiResult(
+              $pageIndex: Int!,
+              $pageSize: Int!,
+              $sortColumn: String,
+              $sortOrder: String,
+              $filterColumn: String,
+              $filterQuery: String) {
+            citiesApiResult(
+              pageIndex: $pageIndex
+              pageSize: $pageSize
+              sortColumn: $sortColumn
+              sortOrder: $sortOrder
+              filterColumn: $filterColumn
+              filterQuery: $filterQuery
+            ) { 
+               data { 
+                 id
+                 name
+                 lat 
+                 lon
+                 countryId
+                 countryName 
+               },
+                pageIndex
+                pageSize
+                totalCount
+                totalPages
+                sortColumn
+                sortOrder
+                filterColumn
+                filterQuery
+              }
+          }
+        `,
+        variables: {
+          pageIndex,
+          pageSize,
+          sortColumn,
+          sortOrder,
+          filterColumn,
+          filterQuery
+        }
+      })
+      .pipe(map((result: any) =>
+        result.data.citiesApiResult));
   }
 
   get(id: number): Observable<City> {
@@ -67,9 +101,25 @@ export class CityService
         result.data.cities.nodes[0]));
   }
 
-  put(item: City): Observable<City> {
-    var url = this.getUrl("api/Cities/" + item.id);
-    return this.http.put<City>(url, item);
+  put(input: City): Observable<City> {
+    return this.apollo
+      .mutate({
+        mutation: gql`
+        mutation UpdateCity($city: CityDTOInput!) {
+          updateCity(cityDTO: $city) { 
+            id
+            name
+            lat
+            lon
+            countryId
+          }
+        }
+      `,
+        variables: {
+          city: input
+        }
+      }).pipe(map((result: any) =>
+        result.data.updateCity));
   }
 
   post(item: City): Observable<City> {
